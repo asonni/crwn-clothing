e86eee3ee710afa32f59b2f45f6ce4694dcc49d7
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-10-05 12:39:29 +0200

    Implement code that pulls in the shop data from firestore and stores it into the shop reducer from the shop page

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ const App = ({ currentUser, setCurrentUser }) => {
     unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-
         userRef.onSnapshot(snapShot => {
           setCurrentUser({ id: snapShot.id, ...snapShot.data() });
         });
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,7 +23,7 @@ const ShopPage = ({ match, updateCollections }) => {
 
   useEffect(() => {
     const collectionRef = firestore.collection('collections');
-    collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       setLoading(false);
